f07b325f47435ace8e468a39cce2596c9be693f0
Neko Boi Nick <thakyz@outlook.com>
2021-02-13 08:59:57 -0800

    Create no_old_reddit.user.js

---
+++ no_old_reddit/no_old_reddit.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         No Old Reddit
+// @namespace    NekoBoiNick.Reddit.NoOld
+// @version      0.1
+// @description  Changes links that redirect to old reddit so that they use the new design
+// @author       Neko Boi Nick
+// @match        *://*.reddit.com/*
+// @license      MIT
+// @grant        none
+// @downloadURL  https://github.com/thakyZ/Userscripts/raw/master/no_old_reddit/no_old_reddit.user.js
+// @updateURL    https://github.com/thakyZ/Userscripts/raw/master/no_old_reddit/no_old_reddit.user.js
+// @supportURL   https://github.com/thakyZ/Userscripts/issues
+// @homepageURL  https://github.com/thakyZ/Userscripts
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var x = document.getElementsByTagName("a");
+    var regex = new RegExp("^https?:\\/\\/(np|old)\.reddit\.com","g")
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].href = x[i].href.replace(regex,"https://reddit.com");
+    }
+})();
