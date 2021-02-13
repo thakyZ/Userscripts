70dbb1f0f511888bfdec91c626d94dc846c96edd
Neko Boi Nick <thakyz@outlook.com>
2021-02-13 09:13:25 -0800

    Changed jquery source

--- mhw_wiki_remove_twitch/mhw_wiki_remove_twitch.user.js
+++ mhw_wiki_remove_twitch/mhw_wiki_remove_twitch.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Monster Hunter World Wiki Remove Twitch Stream
 // @namespace    NekoBoiNick.MonsterHunterWorld.Wiki.RemoveTwitch
-// @version      0.1
+// @version      0.2
 // @description  Removes twitch steam and ads from the Monster Hunter World Wiki
 // @author       NekoBoiNick
 // @match        https://monsterhunterworld.wiki.fextralife.com/*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.x-git.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @license      MIT
 // @downloadURL  https://github.com/thakyZ/Userscripts/raw/master/mhw_wiki_remove_twitch/mhw_wiki_remove_twitch.user.js
 // @updateURL    https://github.com/thakyZ/Userscripts/raw/master/mhw_wiki_remove_twitch/mhw_wiki_remove_twitch.user.js
