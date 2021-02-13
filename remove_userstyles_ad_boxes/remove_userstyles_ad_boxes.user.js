4972d6659d3197e66875023349552ba92fbb77b0
Neko Boi Nick <thakyz@outlook.com>
2021-02-13 09:00:35 -0800

    Added license tag

--- remove_userstyles_ad_boxes/remove_userstyles_ad_boxes.user.js
+++ remove_userstyles_ad_boxes/remove_userstyles_ad_boxes.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Remove UserStyles Ad Boxes
 // @namespace    NekoBoiNick.UserStyles.Ads
-// @version      0.1
+// @version      0.2
 // @description  Removes divs for ads on UserStyles
 // @author       NekoBoiNick
 // @match        https://userstyles.org/*
 // @grant        none
+// @license      MIT
 // @run-at       document-end
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
