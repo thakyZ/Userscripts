04c739b10b0516558fe0dafbd1fd4f9d0b8f721a
Neko Boi Nick <thakyz@outlook.com>
2021-02-13 09:12:24 -0800

    Create steam_workshop_downloader_ads.user.js

---
+++ steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Steam Workshop Downloader Ad Blocker
+// @namespace    nekoboinick.steamworkshopdownloader.adblocker
+// @version      0.1
+// @description  Block ads from appearing on Steam Workshop Downloader
+// @author       thakyZ
+// @match        https://*steamworkshopdownloader.io/*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @license      MIT
+// @downloadURL  https://github.com/thakyZ/Userscripts/raw/master/steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
+// @updateURL    https://github.com/thakyZ/Userscripts/raw/master/steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
+// @supportURL   https://github.com/thakyZ/Userscripts/issues
+// @homepageURL  https://github.com/thakyZ/Userscripts
+// ==/UserScript==
+/* global $, waitForKeyElements */
+
+(function() {
+  $(document).ready(function(){
+    'use strict';
+    var mainContainer = $("main.container");
+
+    var adBoard = mainContainer.children(".row[style='opacity: 0.93;']");
+
+    if (adBoard.length)
+    {
+      adBoard.hide();
+    }
+  });
+})();
