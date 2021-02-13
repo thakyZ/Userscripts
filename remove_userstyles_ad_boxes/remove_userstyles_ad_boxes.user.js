21d544b13783ba2aeceefcfcbde8593f331cda55
Neko Boi Nick <thakyz@outlook.com>
2021-02-13 08:56:18 -0800

    Create remove_userstyles_ad_boxes.user.js

---
+++ remove_userstyles_ad_boxes/remove_userstyles_ad_boxes.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Remove UserStyles Ad Boxes
+// @namespace    NekoBoiNick.UserStyles.Ads
+// @version      0.1
+// @description  Removes divs for ads on UserStyles
+// @author       NekoBoiNick
+// @match        https://userstyles.org/*
+// @grant        none
+// @run-at       document-end
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @downloadURL  https://github.com/thakyZ/Userscripts/raw/master/remove_userstyles_ad_boxes/remove_userstyles_ad_boxes.user.js
+// @updateURL    https://github.com/thakyZ/Userscripts/raw/master/remove_userstyles_ad_boxes/remove_userstyles_ad_boxes.user.js
+// @supportURL   https://github.com/thakyZ/Userscripts/issues
+// @homepageURL  https://github.com/thakyZ/Userscripts
+// ==/UserScript==
+//- The @grant directives are needed to restore the proper sandbox.
+/* global $, waitForKeyElements */
+
+waitForKeyElements (".fallbackDiv", removeFallback, true);
+
+function removeFallback (jNode) {
+  var elements = document.querySelectorAll(".us-stylecard--short .fallbackDiv");
+  elements.forEach(function(e) {
+    e.parentElement.setAttribute("style","display:none;");
+  });
+}
