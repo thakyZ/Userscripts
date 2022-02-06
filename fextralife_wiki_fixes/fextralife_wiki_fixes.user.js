b0dd57edc12e3e69a0365f3c8d29fc34dfa7a5a6
Neko Boi Nick <thakyz@outlook.com>
2022-02-06 10:32:23 -0800

    Update fextralife_wiki_fixes.user.js

--- fextralife_wiki_fixes/fextralife_wiki_fixes.user.js
+++ fextralife_wiki_fixes/fextralife_wiki_fixes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         fExtra Life Wiki Fixes
 // @namespace    NekoBoiNick.FExtraLife.Wiki.Fixes
-// @version      1.0
+// @version      1.0.1
 // @description  Trys to fix some issues about fExtra Life's wiki pages.
 // @author       NekoBoiNick
 // @match        https://*.wiki.fextralife.com/*
@@ -18,18 +18,52 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
+//  'use strict';
 
-  var searchClass = "gsc-results-wrapper-visible";
-  var otherSearchClass = "gsc-results-wrapper-nooverlay";
+  var otherSearchClass = "#sidebar-wrapper";
+  var checked_for_ads = false;
+  var adsHidden = 0;
+  var debug = false;
 
-  window.onhashchange = function() {
-    [...document.getElementsByClassName(otherSearchClass)].forEach((element, index, array) => {
-      if (!element.classList.contains(searchClass))
-      {
-        GM_log("Found broken page");
-        location.reload();
-      }
-    });
+  function getAds() {
+    return document.querySelector(otherSearchClass);
   }
+
+  function hideAd(ad) {
+        ad.remove();
+      adsHidden += 1;
+      if (debug) {
+        console.log('Ads hidden: '+ adsHidden.toString());
+      }
+  }
+
+  setInterval(() => {
+    if (checked_for_ads) {
+      return;
+    }
+
+    const ads = getAds();
+
+    if (ads.length) {
+      Array.from(ads).forEach(hideAd);
+      checked_for_ads = true;
+    }
+  }, 500);
+
+  //window.onhashchange = function() {
+  //  [...document.getElementsByTagName(otherSearchClass)].forEach((element, index, array) => {
+  //    if (checked_for_ads) {
+  //      return;
+  //    }
+  //
+  //    const ads = getAds();
+  //
+  //    if (ads.length) {
+  //      Array.from(ads).forEach(hideAd);
+  //      checked_for_ads = true;
+  //    }
+  //  });
+  //}
+
+  document.addEventListener('scroll', () => Array.from(getAds()).forEach(hideAd));
 })();
