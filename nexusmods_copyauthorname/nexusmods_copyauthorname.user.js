4b63f0d3946720f4b716e8486f5c29be700d668e
Neko Boi Nick <thakyz@outlook.com>
2022-06-02 08:44:46 -0700

    Create nexusmods_copyauthorname.user.js

---
+++ nexusmods_copyauthorname/nexusmods_copyauthorname.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Nexus Mods Copy Author Name
+// @namespace    NekoBoiNick.Web.NexusMods.CopyAuthorName
+// @version      1.0.0
+// @description  Adds a copy author name button to Nexus Mods mod page.
+// @author       Neko Boi Nick
+// @match        https://www.nexusmods.com/*/mods/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nexusmods.com
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/thakyz/Userscripts/master/nexusmods_copyauthorname/nexusmods_copyauthorname.user.js
+// @updateURL    https://raw.githubusercontent.com/thakyz/Userscripts/master/nexusmods_copyauthorname/nexusmods_copyauthorname.user.js
+// @supportURL   https://github.com/thakyZ/Userscripts/issues
+// @homepageURL  https://github.com/thakyZ/Userscripts
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
