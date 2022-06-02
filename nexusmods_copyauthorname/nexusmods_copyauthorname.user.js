cf038363d85be969d013b6b0ca0f005d4894f39f
Neko Boi Nick <thakyz@outlook.com>
2022-06-02 09:24:52 -0700

    Update nexusmods_copyauthorname.user.js

--- nexusmods_copyauthorname/nexusmods_copyauthorname.user.js
+++ nexusmods_copyauthorname/nexusmods_copyauthorname.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nexus Mods Copy Author Name
 // @namespace    NekoBoiNick.Web.NexusMods.CopyAuthorName
-// @version      1.0.0
+// @version      1.0.1
 // @description  Adds a copy author name button to Nexus Mods mod page.
 // @author       Neko Boi Nick
 // @match        https://www.nexusmods.com/*/mods/*
@@ -12,9 +12,34 @@
 // @supportURL   https://github.com/thakyZ/Userscripts/issues
 // @homepageURL  https://github.com/thakyZ/Userscripts
 // ==/UserScript==
+/* global $ */
 
-(function() {
-    'use strict';
-
-    // Your code here...
-})();
+$(document).ready(function() {
+  var creatorInfo = $("#fileinfo .sideitem")[2];
+  var uploaderInfo = $("#fileinfo .sideitem")[3];
+  var infos = [ creatorInfo, uploaderInfo ];
+  var createObjects = function(e, i) {
+    var tempButton = `<style>#action-${e}{position: relative;margin-left: 100px;margin-top: -35px;}#action-${e}::marker{content:none;}#action-copycreatorname .inline-flex .icon{margin: 0 2px 0 2px;}</style>
+    <li style="" id="action-${e}">
+      <a class="btn inline-flex" href="#" tabindex="0">
+        <svg title="" class="icon icon-files">
+          <use xlink:href="https://www.nexusmods.com/assets/images/icons/icons.svg#icon-files"></use>
+        </svg>
+        <span class="flex-label"></span>
+      </a>
+    </li>`
+    var test = $(infos[i]).html();
+    $(infos[i]).html(`${test}\n${tempButton}`);
+    $(`#action-${e} a`).on("click", function(e) {
+      var parent = $(document).find($(e.currentTarget)).parent();
+      var text = $(parent).parent().text().split("\n")[2].replace(" ", "");
+      var $temp = $("<input>");
+      $("body").append($temp);
+      $temp.val(text).select();
+      document.execCommand("copy");
+      $temp.remove();
+    });
+  }
+  createObjects("copycreatorname", 0);
+  createObjects("copyuploadername", 1);
+});
