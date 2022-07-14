6b655a68a5fde8ea9f561a886e4a243af1547cf6
Neko Boi Nick <thakyz@outlook.com>
2022-07-14 07:52:28 -0700

    Create ffxovsquadron_saveload.user.js

---
+++ ffxovsquadron_saveload/ffxovsquadron_saveload.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         FFXIV Squadron App
+// @namespace    NekoBoiNick.Web.FFXIVSquadron.SaveLoad
+// @version      1.0.0
+// @description  Syncs Fish Tracking to soupcat
+// @author       Neko Boi Nick
+// @match        http://ffxivsquadron.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ffxivsquadron.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_registerMenuCommand
+// @downloadURL  https://raw.githubusercontent.com/thakyz/Userscripts/master/ffxivsquadron_saveload/ffxivsquadron_saveload.user.js
+// @updateURL    https://raw.githubusercontent.com/thakyz/Userscripts/master/ffxivsquadron_saveload/ffxivsquadron_saveload.user.js
+// @supportURL   https://github.com/thakyZ/Userscripts/issues
+// @homepageURL  https://github.com/thakyZ/Userscripts
+// ==/UserScript==
+
+/*
+Do not use this file as I had named it wrong, please update to the new mod at 
+https://raw.githubusercontent.com/thakyz/Userscripts/master/ffxivsquadron_saveload/ffxivsquadron_saveload.user.js
+*/
