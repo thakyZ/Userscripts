a47c9760d91754a9adb7699f2b98b4c822a67437
Neko Boi Nick <thakyz@outlook.com>
2022-08-28 09:18:44 -0700

    Update curseforge_redirect.user.js

--- curseforge_redirect/curseforge_redirect.user.js
+++ curseforge_redirect/curseforge_redirect.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name            Curse Forge Redirect
 // @namespace       NekoBoiNick.Curseforge.Redirect
-// @version         0.1
+// @version         1.0.0
 // @description     Redirect Method to redirect to the better curse forge page dedicated to the game you are looking at.
 // @author          Neko Boi Nick
 // @match           *://www.curseforge.com/minecraft/*
 // @match           *://minecraft.curseforge.com/mc-mods/*
+// @match           *://minecraft.curseforge.com/*
+// @match           *://curseforge.com/*
+// @match           *://www.curseforge.com/*
 // @exclude         *://minecraft.curseforge.com/projects/
+// @require         https://code.jquery.com/jquery-3.6.0.min.js
 // @icon            https://github.com/thakyZ/Userscripts/raw/master/curseforge_redirect/curse-icon.png
+// @license         MIT
 // @downloadURL     https://raw.githubusercontent.com/thakyz/Userscripts/master/curseforge_redirect/curseforge_redirect.user.js
 // @updateURL       https://raw.githubusercontent.com/thakyz/Userscripts/master/curseforge_redirect/curseforge_redirect.user.js
 // @supportURL      https://github.com/thakyZ/Userscripts/issues
@@ -15,22 +20,30 @@
 // @grant           none
 // @noframes
 // ==/UserScript==
+/* global $, jQuery */
+this.$ = this.jQuery = jQuery.noConflict(true);
 
-(function() {
-  'use strict';
-  var regexp1 = /^(https?:\/\/)(www)(\.curseforge\.com\/)(minecraft\/)(.+)\/(.+)$/i;
-  var regexp2 = /^(https?:\/\/)(minecraft)(\.curseforge\.com\/)(mc-mods\/)([0-9]+)\-(.+)$/i;
+// Disable any banner with giveaways, as I personally do not want to sign up for them. But only if they contain "Win Awesome Prizes" otherwise others are fine.
+let disable_banner = true;
 
-  var loc = window.location.href;
-
-  var urlrep = "";
-
+$(document).ready(function () {
+  const regexp1 = /^(https?:\/\/)(www)(\.curseforge\.com\/)(minecraft\/)(.+)\/(.+)$/i;
+  const regexp2 = /^(https?:\/\/)(minecraft)(\.curseforge\.com\/)(mc-mods\/)([0-9]+)\-(.+)$/i;
+  const loc = window.location.href;
+  let urlrep = "";
   if (regexp2.test(loc)) {
     urlrep = loc.replace(regexp2, "$1www$3minecraft/mc-mods/$6");
     window.location.href = urlrep;
-  }
-  else if (regexp2.test(loc)) {
+  } else if (regexp2.test(loc)) {
     urlrep = loc.replace(regexp2, "$1$2$3$4$5$6");
     window.location.href = urlrep;
   }
-})();
+  const disableBanner = () => {
+    if ($("html head ~ div a").text().includes("win awesome prizes")) {
+      $("html head ~ div").css({"display": "none"});
+    }
+  };
+  if (disable_banner) {
+    disableBanner();
+  }
+});
