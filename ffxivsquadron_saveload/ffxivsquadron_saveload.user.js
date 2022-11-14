9c500ca59c3190dd902a83c59e8a656b23e1b269
Neko Boi Nick <thakyz@outlook.com>
2022-11-14 09:06:56 -0800

    Update ffxivsquadron_saveload.user.js

--- ffxivsquadron_saveload/ffxivsquadron_saveload.user.js
+++ ffxivsquadron_saveload/ffxivsquadron_saveload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FFXIV Squadron App
 // @namespace    NekoBoiNick.Web.FFXIVSquadron.SaveLoad
-// @version      1.0.4
+// @version      1.0.5
 // @description  Syncs the FFXIV Sqadron App Data
 // @author       Neko Boi Nick
 // @match        https://www.ffxivsquadron.com/*
@@ -13,6 +13,7 @@
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
 // @grant        GM_registerMenuCommand
 // @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @require      https://raw.githubusercontent.com/SloaneFox/code/master/GM4_registerMenuCommand_Submenu_JS_Module.js
@@ -25,8 +26,9 @@
 /* global $, sqMissionSolver, _, jQuery, MonkeyConfig */
 this.$ = this.jQuery = jQuery.noConflict(true);
 
-var SettingsSaveName = "FFXIVSquadronApp.Settings";
-var DateSaveName = "FFXIVSquadronApp.Date";
+const SettingsSaveName = "FFXIVSquadronApp.Settings";
+const DateSaveName = "FFXIVSquadronApp.Date";
+const IconUrlSaveName = "FFXIVSquadronApp.IronUrl";
 
 $(document).ready(function() {
   let getSettings = function() {
@@ -98,4 +100,137 @@ $(document).ready(function() {
   GM_registerMenuCommand("Save Settings", function() {
     saveSettings(true);
   });
+
+  let currentSealsImage = [];
+
+  const getBase64Image = (url, callback) => {
+    GM_xmlhttpRequest({method:"GET",url:url,responseType:"arraybuffer",onload:function(data){
+      if(data.response.length < 1){
+        callback(null, {event:event});
+        return;
+      }
+      // Create an array of 8-bit unsigned integers
+      var arr = new Uint8Array(data.response);
+
+      // String.fromCharCode returns a 'string' from the specified sequence of Unicode values
+      var raw = String.fromCharCode.apply(null, arr);
+
+      //btoa() creates a base-64 encoded ASCII string from a String object
+      var b64 = btoa(raw);
+
+      var dataType = 'png';
+      //ta-da your image data url!
+      var dataURL = 'data:image/' + dataType + ';base64,' + b64;
+      callback(dataURL);
+    },onerror:function(data){
+      callback(null, {event:data});
+    },headers:{referer:"https://www.ffxivsquadron.com/",origin:"https://www.ffxivsquadron.com/"}});
+  }
+
+  const SetupCurrentCrafters = () => {
+    let pages = 0;
+    let currencyId = 0;
+    let itemIconUrl = "";
+    let exit = false;
+    const storedImage = GM_getValue(IconUrlSaveName);
+    if (storedImage !== undefined && storedImage.length > 0) {
+      currentSealsImage = storedImage;
+      return {ExitState:true,ExitCode:1};
+    }
+    let images = [];
+    $.ajax({type:"GET",url:"https://xivapi.com/CollectablesShopRewardScrip",dataType:"json",success:function(data){
+      pages = ((parseInt(data.Pagination.PageTotal) - 1) * 100) + 1;
+      $.ajax({type:"GET",url:`https://xivapi.com/CollectablesShopRewardScrip/${pages}`,dataType:"json",success:function(data){
+        currencyId = parseInt(data.Currency);
+        $.ajax({type:"GET",url:`https://xivapi.com/Currency/${currencyId}`,dataType:"json",success:function(data){
+          itemIconUrl = `https://xivapi.com${data.Item.IconHD}`;
+          getBase64Image(itemIconUrl,function(data, error){
+            if (error) {
+              console.error({message:`Failed to parse image: \`${itemIconUrl}\``,data:data,error:error});
+              exit = true;
+              return;
+            }
+            images.push(data);
+            $.ajax({type:"GET",url:`https://xivapi.com/Currency/${currencyId + 1}`,dataType:"json",success:function(data){
+              itemIconUrl = `https://xivapi.com${data.Item.IconHD}`;
+              getBase64Image(itemIconUrl,function(data, error){
+                if (error) {
+                  console.error({message:`Failed to parse image: \`${itemIconUrl}\``,data:data,error:error});
+                  exit = true;
+                  return;
+                }
+                images.push(data);
+                currentSealsImage = images;
+                GM_setValue(IconUrlSaveName, images);
+              });
+            },error:function(jqXHR, textStatus, errorThrown){
+              console.error({message:`Failed to get Currency with ID: ${currencyId}`,data:jqXHR,error:errorThrown,response:textStatus});
+              exit = true;
+            }});
+            if (exit) {
+              return {ExitState:false,ExitCode:2};
+            }
+          });
+        },error:function(jqXHR, textStatus, errorThrown){
+          console.error({message:`Failed to get Currency with ID: ${currencyId}`,data:jqXHR,error:errorThrown,response:textStatus});
+          exit = true;
+        }});
+      },error:function(jqXHR, textStatus, errorThrown){
+        console.error({message:`Failed to get Collectables Reward Scrip with ID: ${pages}`,data:jqXHR,error:errorThrown,response:textStatus});
+        exit = true;
+      }});
+    },error:function(jqXHR, textStatus, errorThrown){
+      console.error({message:"Failed to get Collectables Reward Scrips",data:jqXHR,error:errorThrown,response:textStatus});
+      exit = true;
+    }});
+    if (exit) {
+      return {ExitState:false,ExitCode:1};
+    }
+    return {ExitState:true,ExitCode:0};
+  }
+
+  const SetupMutationObserver = () => {
+    const targetNode = $("#content-right")[0];
+    const config = { attributes: false, childList: true, subtree: true };
+    const updateIcon = SetupCurrentCrafters();
+
+    if (updateIcon.ExitState === false) {
+      return;
+    }
+
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0 && $(mutation.addedNodes[0]).attr("class").split(" ")[0] === "result-line") {
+          for (let i = 0; i < mutation.addedNodes.length; i++) {
+            const node = $(mutation.addedNodes[i]);
+            const chemistries = $(node).children(".stats-and-training").children(".chemistries");
+            if (chemistries.length > 0) {
+              const activeChemistries = $(chemistries).children("div:not(.title-stat)");
+              for (let j = 0; j < activeChemistries.length; j++) {
+                const chemImage = $(".img-chem img", $(activeChemistries[j]))
+                if ($(chemImage).attr("src") === "images/cra_scrips.png") {
+                  $(chemImage).attr("src", `${currentSealsImage[1]}`);
+                  $(chemImage).attr("width","32");
+                  $(chemImage).attr("height","32");
+                }
+                if ($(chemImage).attr("src") === "images/gat_scrips.png") {
+                  $(chemImage).attr("src", `${currentSealsImage[1]}`);
+                  $(chemImage).attr("width","32");
+                  $(chemImage).attr("height","32");
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+
+    $(document).on("unload", function() {
+      observer.disconnect();
+    });
+  };
+
+  SetupMutationObserver();
 });
