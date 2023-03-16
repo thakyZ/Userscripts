921156cbf8baaeaa00b9575cf327434d275e9482
thakyZ <thakyz@outlook.com>
2023-03-16 10:05:48 -0700

    Updated Twitch Remove Prime Notifications

--- twitch_prime_notifs/twitch_prime_notifs.user.js
+++ twitch_prime_notifs/twitch_prime_notifs.user.js
@@ -1,91 +1,99 @@
 // ==UserScript==
 // @name         Twitch Remove Prime Notifications
 // @namespace    NekoBoiNick.Web.TwitchPrime.NotifRM
-// @version      1.0.1
+// @version      1.0.2
 // @description  Remove Twitch's notifications for Prime.
 // @author       Neko Boi Nick
 // @match        https://www.twitch.tv/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
-// @grant        none
 // @license      MIT
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/thakyz/Userscripts/master/twitch_prime_notifs/twitch_prime_notifs.user.js
 // @updateURL    https://raw.githubusercontent.com/thakyz/Userscripts/master/twitch_prime_notifs/twitch_prime_notifs.user.js
 // @supportURL   https://github.com/thakyZ/Userscripts/issues
 // @homepageURL  https://github.com/thakyZ/Userscripts
 // ==/UserScript==
+/* global $, jQuery */
+this.$ = this.jQuery = jQuery.noConflict(true);
 
-(function() {
-  'use strict';
+$(document).ready(() => {
+  "use strict";
 
-  var otherSearchClasses = [".prime-offers > div:last-child",".prime-offers__pill"];
-  var twitchFrontPageSystem = ".featured-content-carousel__item-container--center video";
-  var checked_for_classes = false;
-  var classesHidden = 0;
-  var debug = false;
+  const otherSearchClasses = [".prime-offers > div:last-child", ".prime-offers__pill"];
+  const twitchFrontPageSystem = ".featured-content-carousel__item-container--center video";
+  let checkedForClasses = false;
+  let classesHidden = 0;
+  const debug = false;
 
-  var getClasses = function() {
-    var test = [];
-    for (var i = 0; i < otherSearchClasses.length; i++)
-    {
-      var test2 = document.querySelector(otherSearchClasses[i]);
-      if (test2 !== null) {
-        test.push(test2);
+  const getClasses = () => {
+    const test = [];
+    for (const [, searchClass] of Object.entries(otherSearchClasses)) {
+      if (searchClass !== null) {
+        test.push($(searchClass));
       }
     }
-    return test;
-  }
 
-  var hideClass = function(_class) {
+    return test;
+  };
+
+  const hideClass = _class => {
     _class.style.cssText += "display:none";
     classesHidden += 1;
     if (debug) {
-      console.log('Ads hidden: ', classesHidden.toString());
+      console.log("Ads hidden: ", classesHidden.toString());
     }
-  }
+  };
 
   setInterval(() => {
-    if (checked_for_classes) return;
-    var classes = getClasses();
+    if (checkedForClasses) {
+      return;
+    }
+
+    const classes = getClasses();
     if (classes.length > 0) {
-      classes.forEach((obj, inx, arr) => {
-        if (arr.length > 0) {
-          hideClass(obj);
-        }
+      classes.forEach(obj => {
+        hideClass(obj);
       });
-      checked_for_classes = true;
-    }}, 1000);
+      checkedForClasses = true;
+    }
+  }, 1000);
 
-  setInterval(() => { if (checked_for_classes) checked_for_classes = false; }, 5000);
+  setInterval(() => {
+    if (checkedForClasses) {
+      checkedForClasses = false;
+    }
+  }, 5000);
 
-  var pauseMainVideo = function() {
+  const pauseMainVideo = function () {
     if (window.location.href.match(/^https?:\/\/(www.)?twitch.tv(\/)?$/)) {
-      var video = document.querySelector(twitchFrontPageSystem);
-      video.onplay = function() {
+      const video = document.querySelector(twitchFrontPageSystem);
+      video.onplay = function () {
         video.pause();
       };
     }
-  }
-
-  var waited = false;
+  };
 
   let id = -1;
-  id = setInterval(function(){
-    let div = document.querySelector(twitchFrontPageSystem);
-    if (div) {
+  id = setInterval(() => {
+    if ($(twitchFrontPageSystem).length > 0) {
       pauseMainVideo();
       clearInterval(id);
     }
   }, 2500);
 
-  var runCommands = function() {
-    [...Array.from(getClasses()).forEach((element, index, array) => {
-      if (element.length) {
-        Array.from(element).forEach(hideClass);
-      }})]
+  const runCommands = () => {
+    $(getClasses()).each((_, element) => {
+      $(element).each(() => hideClass());
+    });
     pauseMainVideo();
-  }
+  };
 
-  window.onhashchange = function() {runCommands();}
+  $(window).on("hashchange", () => {
+    runCommands();
+  });
 
-  document.addEventListener('scroll', () => {runCommands();});
-})();
+  $(document).on("scroll", () => {
+    runCommands();
+  });
+});
