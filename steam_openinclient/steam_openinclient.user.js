79a69fa3b8c7e0426debf9ed361f690b23e9f8d0
thakyZ <thakyz@outlook.com>
2023-03-16 09:26:25 -0700

    Updated Open URL in Steam Button

--- steam_openinclient/steam_openinclient.user.js
+++ steam_openinclient/steam_openinclient.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open URL in Steam Button
 // @namespace    NekoBoiNick.Web.Steam.OpenInClient
-// @version      1.0.0
+// @version      1.0.1
 // @description  Adds a button to open pages in the Steam client.
 // @author       Neko Boi Nick
 // @match        https://steamcommunity.com/*
@@ -15,46 +15,27 @@
 // @supportURL   https://github.com/thakyZ/Userscripts/issues
 // @homepageURL  https://github.com/thakyZ/Userscripts
 // ==/UserScript==
-/* global $, jQuery, MonkeyConfig */
+/* global $, jQuery */
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 $(document).ready(() => {
   if (!window.location.href.match(/^https:\/\/(store\.)?steam(powered|community)\.com\//)) {
     return;
   }
-  var CreateOpenButton = () => {
-    return `
-<div class="open-in-steam">
-  <a rel="noopener" style="" class="btnv6_blue_hoverfade btn_medium" id="open-in-steam_button">
-    <span>
-      <img class="ico16" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9IiM2NkMwRjQiIHZpZXdCb3g9IjMgMyAxOCAxOCI+PHBhdGggZD0iTTEyLDEwTDgsMTRIMTFWMjBIMTNWMTRIMTZNMTksNEg1QzMuODksNCAzLDQuOSAzLDZWMThBMiwyIDAgMCwwIDUsMjBIOVYxOEg1VjhIMTlWMThIMTVWMjBIMTlBMiwyIDAgMCwwIDIxLDE4VjZBMiwyIDAgMCwwIDE5LDRaIiAvPjwvc3ZnPg==">
-    </span>
-  </a>
-</div>
-    `;
-  };
-  var Styles = () => {
-    return `
-.open-in-steam #open-in-steam_button span .ico16 {
-  background: none;
-}
-.open-in-steam {
-    margin-left: 10px;
-    display: inline-block;
-    vertical-align: 12px;
-}
-    `;
-  };
-  var steamOpenUrl = "steam://openurl/";
-  var globalHeader = $("#global_header .content");
-  var openButton = CreateOpenButton();
-  var styles = document.createElement("style");
-  $(styles).text(Styles());
-  $("head").append(styles);
+
+  const createOpenButton = () => "<div class=\"open-in-steam\"><a rel=\"noopener\" style=\"\" class=\"btnv6_blue_hoverfade btn_medium\" id=\"open-in-steam_button\"><span><img class=\"ico16\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9IiM2NkMwRjQiIHZpZXdCb3g9IjMgMyAxOCAxOCI+PHBhdGggZD0iTTEyLDEwTDgsMTRIMTFWMjBIMTNWMTRIMTZNMTksNEg1QzMuODksNCAzLDQuOSAzLDZWMThBMiwyIDAgMCwwIDUsMjBIOVYxOEg1VjhIMTlWMThIMTVWMjBIMTlBMiwyIDAgMCwwIDIxLDE4VjZBMiwyIDAgMCwwIDE5LDRaIiAvPjwvc3ZnPg==\"></span></a></div>";
+  const newStyles = () => ".open-in-steam #open-in-steam_button span .ico16 {background: none;}.open-in-steam {margin-left: 10px;display: inline-block;vertical-align: 12px;}";
+  const steamOpenUrl = "steam://openurl/";
+  const globalHeader = $("#global_header .content");
+  const openButton = createOpenButton();
+  const styles = document.createElement("style");
+  $(styles).text(newStyles());
+  $("head").append(newStyles);
   if ($("#global_actions .user_avatar.playerAvatar").length > 0) {
     $("#global_actions .user_avatar.playerAvatar").after(openButton);
   } else {
     $(globalHeader).append(openButton);
   }
+
   $(".open-in-steam #open-in-steam_button").attr("href", `${steamOpenUrl}${window.location.href}`);
 });
