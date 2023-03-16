960e3deca0740f98f44df8003c24cdf63151bfca
thakyZ <thakyz@outlook.com>
2023-03-16 09:30:52 -0700

    Updated Steam Workshop Downloader Ad Blocker

--- steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
+++ steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
@@ -1,31 +1,26 @@
 // ==UserScript==
 // @name         Steam Workshop Downloader Ad Blocker
 // @namespace    NekoBoiNick.SteamWorkshopDownloader.AdBlocker
-// @version      0.3
+// @version      1.0.0
 // @description  Block ads from appearing on Steam Workshop Downloader
 // @author       Neko Boi Nick
 // @match        https://*steamworkshopdownloader.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamworkshopdownloader.io
-// @grant        none
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @license      MIT
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/thakyz/Userscripts/master/steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
 // @updateURL    https://raw.githubusercontent.com/thakyz/Userscripts/master/steam_workshop_downloader_ads/steam_workshop_downloader_ads.user.js
 // @supportURL   https://github.com/thakyZ/Userscripts/issues
 // @homepageURL  https://github.com/thakyZ/Userscripts
 // ==/UserScript==
-/* global $, waitForKeyElements */
+/* global $, jQuery */
+this.$ = this.jQuery = jQuery.noConflict(true);
 
-(function() {
-  $(document).ready(function(){
-    'use strict';
-    var mainContainer = $("main.container");
-
-    var adBoard = mainContainer.find(".row.p-1");
-
-    if (adBoard.length)
-    {
-      adBoard.hide();
-    }
-  });
-})();
+$(document).ready(() => {
+  "use strict";
+  const adBoard = $("main.container .row.p-1");
+  if (adBoard.length > 0) {
+    adBoard.hide();
+  }
+});
