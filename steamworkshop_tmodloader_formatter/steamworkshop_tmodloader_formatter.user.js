b16b91039698e8ded426759a20fe0a735df63d5d
thakyZ <thakyz@outlook.com>
2023-03-16 09:50:40 -0700

    Updated TModLoader Workshop Page Formatter

--- steamworkshop_tmodloader_formatter/steamworkshop_tmodloader_formatter.user.js
+++ steamworkshop_tmodloader_formatter/steamworkshop_tmodloader_formatter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TModLoader Workshop Page Formatter
 // @namespace    NekoBoiNick.Web.Steam.Workshop.TModLoader
-// @version      1.0.0
+// @version      1.0.1
 // @description  Format TModLoader workshop pages.
 // @author       Neko Boi Nick
 // @match        https://steamcommunity.com/sharedfiles/filedetails/*
@@ -23,68 +23,66 @@
 // ==/UserScript==
 /* global $, jQuery, MonkeyConfig */
 this.$ = this.jQuery = jQuery.noConflict(true);
-$(document).ready(function () {
+
+$(document).ready(() => {
   GM_deleteValue("teamworkshop_tmodloader_formatter_data");
-  var config = new MonkeyConfig({
-    title: 'Configure',
+
+  const config = new MonkeyConfig({
+    title: "Configure",
     menuCommand: true,
     params: {
       key: {
         type: "text",
-        default: ""
-      }
-    }
+        default: "",
+      },
+    },
   });
-  var itemIcons;
-  var checkItemsSet = function (e0, cb) {
-    var test = () => { var value = config.get("key"); if (value !== undefined && value !== "") { return value } else { return false } };
-    var test2 = test();
-    if (test2 === false) {
-      return null;
-    } else {
-      return sendItems(test2, cb);
-    }
-  }
-  var sendItems = function (e0, cb) {
+  let itemIcons;
+  const checkItemsSet = cb => {
+    const value = config.get("key");
+    return value !== undefined && value !== "" ? sendItems(value, cb) : null;
+  };
+
+  const sendItems = (e0, cb) => {
     $.ajax({
       url: `https://api.nekogaming.xyz/json/get_json.php?key=${e0}&file=steamworkshop_tmodloader_formatter_data`,
-      beforeSend: function (xhr) {
+      beforeSend(xhr) {
         xhr.overrideMimeType("application/json; charset=utf-8");
       },
-      success: function (data) {
+      success(data) {
         cb(null, data);
       },
-      error: function (x, s, e) {
+      error(x, s, e) {
         console.log(x);
         console.log(s);
         console.log(e);
-      }
+      },
     });
   };
-  var styleElement = document.createElement("style");
-  var ParseCSS = function () {
-    $(styleElement).html(".tmodloadericon{width:16px;height:16px;display:inline-block;}");
-  }
-  var ParseCSSIcon = function (id) {
-    if ($(styleElement).html().indexOf(`.tmodloadericon.item-${id}`) !== -1) {
-      return;
+
+  const styleElement = $("<style>.tmodloadericon{width:16px;height:16px;display:inline-block;}</style>");
+  const parseCSSIcon = id => {
+    const currentHtml = $(styleElement).html();
+    return currentHtml.indexOf(`.tmodloadericon.item-${id}`) > -1 ? null : $(styleElement).html(currentHtml.concat(`.tmodloadericon.item-${id}{background-image:url("${itemIcons[id]}");}`));
+  };
+
+  const parseText = () => {
+    const getDescription = $($(".workshopItemDescription")[0]);
+    const regexFont = /\[c\/(\w{6}):((?:[^\][]+))\]/i;
+    const regexIcon = /\[i:(\d{1,4})\]/gi;
+    const test = $(getDescription).html().replaceAll(regexFont, "<span style=\"color:#$1;\">$2</span>");
+    const test2 = [...test.match(regexIcon)];
+    for (const [, match] of Object.entries(test2)) {
+      parseCSSIcon(match[1]);
     }
-    $(styleElement).html($(styleElement).html() + `.tmodloadericon.item-${id}{background-image:url("${itemIcons[id]}");}`);
-  }
-  var ParseText = function () {
-    var getDescription = $($(".workshopItemDescription")[0]);
-    var regexFont = /\[c\/([a-z0-9]{6})\:((?:[^\]\[]+))\]/g;
-    var regexIcon = /\[i:([0-9]{1,4})\]/g;
-    var test = $(getDescription).html().replaceAll(regexFont, `<span style="color:#$1;">$2</span>`);
-    var test2 = [...test.matchAll(regexIcon)];
-    ParseCSS();
-    test2.forEach(function (e, i) {
-      ParseCSSIcon(e[1]);
-    });
-    var test4 = test.replaceAll(regexIcon, `<i class="tmodloadericon item-$1"></i>`);
-    var soup2 = $(getDescription).html();
+
+    const test3 = test.replaceAll(regexIcon, "<i class=\"tmodloadericon item-$1\"></i>");
     $("head").append(styleElement);
-    $(getDescription).html(test4);
-  }
-  checkItemsSet(null, function (e, b) { itemIcons = b; ParseText(); });
+    $(getDescription).html(test3);
+  };
+
+  checkItemsSet((_, b) => {
+    itemIcons = b;
+    parseText();
+  });
 });
