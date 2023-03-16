95e5c2141269fce473e7405c856f5b32beb99704
thakyZ <thakyz@outlook.com>
2023-03-16 09:23:56 -0700

    Updated Speed Test Redirect

--- speed_test_redirect/speed_test_redirect.user.js
+++ speed_test_redirect/speed_test_redirect.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Speed Test Redirect
 // @namespace    NekoBoiNick.Web.Search.SpeedTest
-// @copyright    2021, Neko Boi Nick
-// @version      1.0.0
+// @copyright    2023, Neko Boi Nick
+// @version      1.0.2
 // @license      MIT
 // @description  Redirects the search engine to a proper speed test site.
 // @author       Neko Boi Nick
@@ -20,83 +20,80 @@
 // @grant        GM_registerMenuCommand
 // @run-at       document-start
 // @require      https://raw.githubusercontent.com/SloaneFox/code/master/GM4_registerMenuCommand_Submenu_JS_Module.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @require      https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @downloadURL  https://raw.githubusercontent.com/thakyz/Userscripts/master/speed_test_redirect/speed_test_redirect.user.js
 // @updateURL    https://raw.githubusercontent.com/thakyz/Userscripts/master/speed_test_redirect/speed_test_redirect.user.js
 // @supportURL   https://github.com/thakyZ/Userscripts/issues
 // @homepageURL  https://github.com/thakyZ/Userscripts
 // ==/UserScript==
-/* globals MonkeyConfig */
+/* global $, jQuery, MonkeyConfig */
+this.$ = this.jQuery = jQuery.noConflict(true);
 
-(function() {
-  'use strict';
+$(document).ready(() => {
+  "use strict";
 
-  var config = new MonkeyConfig({
-    title: 'Configure',
+  const config = new MonkeyConfig({
+    title: "Configure",
     menuCommand: true,
     params: {
-      redirect_site: {
+      redirectSite: {
         type: "select",
         choices: ["Measurement Lab", "Ookla", "Fast"],
-        default: "Measurement Lab"
+        default: "Measurement Lab",
       },
-      for_google: {
-        type: 'checkbox',
-        default: true
+      forGoogle: {
+        type: "checkbox",
+        default: true,
       },
-      for_duckduckgo: {
-        type: 'checkbox',
-        default: true
+      forDuckduckgo: {
+        type: "checkbox",
+        default: true,
       },
-      for_bing: {
-        type: 'checkbox',
-        default: true
+      forBing: {
+        type: "checkbox",
+        default: true,
       },
-      for_yahoo: {
-        type: 'checkbox',
-        default: true
+      forYahoo: {
+        type: "checkbox",
+        default: true,
       },
-      for_yandex: {
-        type: 'checkbox',
-        default: true
-      }
+      forYandex: {
+        type: "checkbox",
+        default: true,
+      },
+    },
+  });
+
+  const redirectSites = ["https://speed.measurementlab.net/", "https://speedtest.net", "https://fast.com"];
+  const services = {
+    google: /^(https?:\/\/)((www\.)?)google.com\/search\?(.+)?q=speed(\+)?test(.+)?$/i,
+    duckduckgo: /^(https?:\/\/)((www\.)?)duckduckgo.com\/\?(.+)?q=speed(\+)?test(.+)?$/i,
+    bing: /^(https?:\/\/)((www\.)?)bing.com\/search\?q=speed(%20)?test(.+)?$/i,
+    yahoo: /^(https?:\/\/)search\.yahoo.com\/search(;_ylt=(.+);_ylc=(.+))?\?p=speed(\+)?test(.+)?$/i,
+    yandex: /^(https?:\/\/)(www\.)?yandex.(ru|com)\/search\/\?(.+)?(&)?text=speed(\+)?test(.+)?$/i
+  };
+
+  const loc = window.location.href;
+
+  const fromSiteChoice = () => {
+    switch (config.get("redirectSite")) {
+    case "Measurement Lab":
+      return redirectSites[0];
+    case "Ookla":
+      return redirectSites[1];
+    case "Fast":
+    default:
+      return redirectSites[2];
     }
-  })
+  };
 
-  var redirectSites = ["https://speed.measurementlab.net/", "https://speedtest.net", "https://fast.com"];
+  const capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
 
-  var regexGoogle = /^(https?:\/\/)((www\.)?)google.com\/search\?(.+)?q=speed(\+)?test(.+)?$/i;
-  var regexDuckDuckGo = /^(https?:\/\/)((www\.)?)duckduckgo.com\/\?(.+)?q=speed(\+)?test(.+)?$/i;
-  var regexBing = /^(https?:\/\/)((www\.)?)bing.com\/search\?q=speed(\%20)?test(.+)?$/i;
-  var regexYahoo = /^(https?:\/\/)search\.yahoo.com\/search(\;_ylt=(.+);_ylc=(.+))?\?p=speed(\+)?test(.+)?$/i;
-  var regexYandex = /^(https?:\/\/)(www\.)?yandex.(ru|com)\/search\/\?(.+)?(&)?text=speed(\+)?test(.+)?$/i;
-
-  var loc = window.location.href;
-
-  function from_site_choice() {
-    switch (config.get("redirect_site")) {
-      case "Measurement Lab":
-        return redirectSites[0];
-      case "Ookla":
-        return redirectSites[1];
-      case "Fast":
-        return redirectSites[2];
+  for (const [service, regex] of Object.entries(services)) {
+    if (config.get(`for${capitalizeFirstLetter(service)}`) && regex.test(loc)) {
+      window.location.href = fromSiteChoice();
+      break;
     }
   }
-
-  if (config.get("for_google") && regexGoogle.test(loc)) {
-    window.location.href = from_site_choice();
-  }
-  if (config.get("for_duckduckgo") && regexDuckDuckGo.test(loc)) {
-    window.location.href = from_site_choice();
-  }
-  if (config.get("for_bing") && regexBing.test(loc)) {
-    window.location.href = from_site_choice();
-  }
-  if (config.get("for_yahoo") && regexYahoo.test(loc)) {
-    window.location.href = from_site_choice();
-  }
-  if (config.get("for_yandex") && regexYandex.test(loc)) {
-    window.location.href = from_site_choice();
-  }
-})();
+});
