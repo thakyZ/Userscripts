1e63768effd6d1887b6fff0ca7d9a3435f320c89
thakyZ <nekoboinick@gmail.com>
2023-05-02 08:04:52 -0700

    Updated eslint compile2json

--- eslint_compile2json/eslint_compile2json.mjs
+++ eslint_compile2json/eslint_compile2json.mjs
@@ -5,9 +5,21 @@ import * as eslintConfig from "../eslint.config.mjs";
 
 const outDirectory = path.join(import.meta.url, "..", "..", "out", "eslint.json").toString().replace("file:\\", "");
 
+async function enableAllGlobals(globals) {
+  for (var globalVar in globals) {
+    if (Object.prototype.hasOwnProperty.call(globals, globalVar) && globals[globalVar] === false) {
+      globals[globalVar] = true;
+    }
+  }
+  return globals;
+}
+
 async function loadEslintConfig() {
   const newJsonTree = {};
-  newJsonTree.env = {...eslintConfig.default[0].languageOptions.globals};
+
+  newJsonTree.env = { es6: true, greasemonkey: true, browser: true, jquery: true },
+  newJsonTree.globals = {...eslintConfig.default[0].languageOptions.globals};
+  //newJsonTree.globals = await enableAllGlobals(newJsonTree.globals);
   newJsonTree.extends = [ "eslint:recommended" ];
   newJsonTree.parserOptions = {...eslintConfig.default[0].languageOptions.parserOptions};
   newJsonTree.rules = {...eslintConfig.default[0].rules};
